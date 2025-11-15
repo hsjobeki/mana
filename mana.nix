466e23bbd1536f17522c633ee00fe66bcd935be2
Johannes Kirschbauer <hsjobeki@gmail.com>
2025-11-15 17:03:00 +0700

    clean up manafest

--- mana.nix
+++ mana.nix
@@ -1,17 +1,6 @@
-rec {
+{
   entrypoint = ./entrypoint.nix;
   dependencies = {
     nixpkgs.url = "github:nixos/nixpkgs";
-  };
-  transitiveOverrides = {
-    nixpkgs = dependencies.nixpkgs;
-  };
-  groups = {
-    eval = {
-      nixpkgs = [ ];
-      dep1 = [ "b1" "b1" ];
-    };
-    dev = {
-    };
   };
 }
