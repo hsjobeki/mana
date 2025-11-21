8fdb4339d97ecf0c6458975dc6c20e65a13b6ae3
Johannes Kirschbauer <hsjobeki@gmail.com>
2025-11-21 15:47:54 +0700

    deps: use nixos-unstable for development

--- mana.nix
+++ mana.nix
@@ -1,6 +1,17 @@
+# mana manifest
 {
   entrypoint = ./entrypoint.nix;
   dependencies = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  groups = {
+    eval = {
+      # Mana will become nixpkgs-free
+      nixpkgs = [ ];
+    };
+    dev = {
+      nixpkgs = [ ];
+    };
   };
 }
