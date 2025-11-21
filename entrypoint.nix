f82baa2bb09d175f690ba7a5b94151225760f800
Johannes Kirschbauer <hsjobeki@gmail.com>
2025-11-21 15:48:27 +0700

    tests: provide shell

--- entrypoint.nix
+++ entrypoint.nix
@@ -1,5 +1,5 @@
 # manifest dependencies, injected by the nix/importer.nix
-{nixpkgs , ... }:
+{nixpkgs }:
 # - to test: nix repl -f default.nix
 # - to pass an explicit system 'import default.nix { system = "x86_64-linux"; }'
 {system ? builtins.currentSystem }:
@@ -8,4 +8,12 @@ let
 in
 {
   mana = pkgs.callPackage ./packages/mana { };
+
+  shell = pkgs.mkShell {
+    packages = [
+      pkgs.nix-unit
+    ];
+  };
+
+  tests = import ./nix/libTests.nix;
 }
